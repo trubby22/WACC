020b78a51c3dd1d56d0c3d0162fda77229d8ce2a
Emilian Filip <emilian.filip20@imperial.ac.uk>
2022-02-04 13:31:19 +0000

    [ef420] [feature] Add symbol table and identifier types. Top-level symbol tables are initialised with some pre-defined basic types and standard WACC functions.
    TODO: Find a way to represent functions that can be overloaded with different type parameters. Add the rest of the standard WACC functions to the top level symbol table.

---
+++ src/main/kotlin/ic/doc/group15/semantics/SymbolTable.kt
@@ -0,0 +1,65 @@
+package ic.doc.group15.semantics
+
+class SymbolTable private constructor(val enclosingTable: SymbolTable?) {
+
+    private val map: MutableMap<String, Identifier> = HashMap()
+
+    companion object {
+        private val basicTypes = mapOf(
+            Pair("int", IntType(min = Int.MIN_VALUE, max = Int.MAX_VALUE)),
+            Pair("float", FloatType(min = Float.MIN_VALUE, max = Float.MAX_VALUE)),
+            Pair("char", CharType(min = 0, max = 255)),
+            Pair("string", StringType()),
+            Pair("bool", BoolType(falseVal = 0, trueVal = 1)),
+        )
+
+        fun topLevel(): SymbolTable {
+            val st = SymbolTable(null)
+            st.map.putAll(basicTypes)
+            val standardFunctions = arrayOf(
+                Pair("print", FunctionType(null, listOf(Param(StringType())), st.subScope())),
+                Pair("println", FunctionType(null, listOf(Param(StringType())), st.subScope())),
+                Pair(
+                    "exit",
+                    FunctionType(
+                        null,
+                        listOf(Param(basicTypes["int"]!!)),
+                        st.subScope()
+                    )
+                ),
+                Pair(
+                    "newpair",
+                    FunctionType(
+                        PairType(Type(), Type()),
+                        listOf(Param(Type()), Param(Type())),
+                        st.subScope()
+                    )
+                )
+            )
+            st.map.putAll(standardFunctions)
+            return st
+        }
+    }
+
+    fun add(name: String, ident: Identifier) {
+        map[name] = ident
+    }
+
+    fun lookup(name: String): Identifier? {
+        return map[name]
+    }
+
+    fun lookupAll(name: String): Identifier? {
+        var st: SymbolTable? = this
+        while (st != null) {
+            var ident = st.lookup(name)
+            if (ident != null) return ident
+            st = st.enclosingTable
+        }
+        return null
+    }
+
+    fun subScope(): SymbolTable {
+        return SymbolTable(this)
+    }
+}
