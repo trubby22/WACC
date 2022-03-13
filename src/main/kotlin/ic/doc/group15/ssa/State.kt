86a25e25a78289380cc2abf7f513421f8376e969
kjc20 <khemi.chew20@imperial.ac.uk>
2022-03-13 17:11:12 +0000

    [kjc20] [feat] introduce layout for ssa construction in program and function ast

---
+++ src/main/kotlin/ic/doc/group15/ssa/State.kt
@@ -0,0 +1,4 @@
+package ic.doc.group15.ssa
+
+class State {
+}
--- src/main/kotlin/ic/doc/group15/visitor/IRGenerator.kt
+++ src/main/kotlin/ic/doc/group15/visitor/IRGenerator.kt
@@ -2,160 +2,205 @@ package ic.doc.group15.visitor
 
 import ic.doc.group15.assembly.TranslatorMethod
 import ic.doc.group15.ast.*
+import ic.doc.group15.ssa.Function
+import ic.doc.group15.ssa.State
+import ic.doc.group15.type.BasicType
 
-class IRGenerator {
+class IRGenerator(private val enableLogging: Boolean = true) {
     companion object {
         private val translators: ASTTranslatorMap = generateASTTranslatorMap(IRGenerator::class)
     }
 
-    private fun translate(node: ASTNode) {
-        IRGenerator.translators[node::class]?.call(this, node)
+    private fun translate(state: State, node: ASTNode): Any? {
+        return translators[node::class]?.call(this, state, node)
     }
 
     @TranslatorMethod(AST::class)
-    private fun translateProgram(program: AST) {
-        TODO()
+    private fun translateProgram(state: State, program: AST): List<Function> {
+        log("Building SSA for program")
+        val (functions, statements) = program.statements.partition { node -> node is FunctionDeclarationAST }
+        // Build SSA form for each function TODO(figure out what state to store or throw away entirely)
+        val ssaFunctions = mutableListOf<Function>()
+
+        functions.forEach { ssaFunctions.add(translate(State(), it) as Function) }
+
+        val ssaMainFunction = translateMain(State(), program, *statements.toTypedArray())
+        ssaFunctions.add(ssaMainFunction)
+        return ssaFunctions
     }
 
+    private fun translateMain(state: State, program: AST, vararg stat: StatementAST): Function {
+        val node = FunctionDeclarationAST(program,
+            program.symbolTable,
+            program.symbolTable.subScope(),
+            BasicType.IntType,
+            "main"
+        )
+        val statements = node.statements
+        // Add all statements
+        statements.addAll(stat)
+        // Add return statement if not found
+        if (statements.isEmpty()
+            || statements.last() !is ReturnStatementAST
+            || statements.last() !is ExitStatementAST) {
+            val returnNode = ReturnStatementAST(node, node.symbolTable, IntLiteralAST(0))
+            statements.add(returnNode)
+        }
+
+        return translate(state, node) as Function
+    }
+
+    /**
+     * 1) Build CFG for each function
+     * 2) Construct dominator tree and compute set of dominance frontier for each basic block
+     * 3) Insert phi nodes
+     * 4) Rename variables to obtain SSA form
+     **/
     @TranslatorMethod(FunctionDeclarationAST::class)
-    private fun translateFunctionDeclaration(node: FunctionDeclarationAST) {
+    private fun translateFunctionDeclaration(state: State, node: FunctionDeclarationAST) {
         TODO()
     }
 
 
     @TranslatorMethod(IfBlockAST::class)
-    private fun translateIfBlock(stat: IfBlockAST) {
+    private fun translateIfBlock(state: State, node: IfBlockAST) {
         TODO()
     }
 
     @TranslatorMethod(WhileBlockAST::class)
-    private fun translateWhileBlock(node: WhileBlockAST) {
+    private fun translateWhileBlock(state: State, node: WhileBlockAST) {
         TODO()
     }
 
     @TranslatorMethod(BeginEndBlockAST::class)
-    private fun translateBeginEndBlock(node: BeginEndBlockAST) {
+    private fun translateBeginEndBlock(state: State, node: BeginEndBlockAST) {
         TODO()
     }
 
     @TranslatorMethod(CallAST::class)
-    private fun translateFunctionCall(node: CallAST) {
+    private fun translateFunctionCall(state: State, node: CallAST) {
         TODO()
     }
 
     @TranslatorMethod(VariableDeclarationAST::class)
-    private fun translateVariableDeclaration(node: VariableDeclarationAST) {
+    private fun translateVariableDeclaration(state: State, node: VariableDeclarationAST) {
         TODO()
     }
 
     @TranslatorMethod(AssignToIdentAST::class)
-    private fun translateAssignToVariable(node: AssignToIdentAST) {
+    private fun translateAssignToVariable(state: State, node: AssignToIdentAST) {
         TODO()
     }
 
     @TranslatorMethod(FreeStatementAST::class)
-    private fun translateFreeStat(node: FreeStatementAST) {
+    private fun translateFreeStat(state: State, node: FreeStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(ReturnStatementAST::class)
-    private fun translateReturnStat(node: ReturnStatementAST) {
+    private fun translateReturnStat(state: State, node: ReturnStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(ExitStatementAST::class)
-    private fun translateExitStat(node: ExitStatementAST) {
+    private fun translateExitStat(state: State, node: ExitStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(PrintStatementAST::class)
-    private fun translatePrintStat(node: PrintStatementAST) {
+    private fun translatePrintStat(state: State, node: PrintStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(PrintlnStatementAST::class)
-    private fun translatePrintlnStat(node: PrintlnStatementAST) {
+    private fun translatePrintlnStat(state: State, node: PrintlnStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(ReadStatementAST::class)
-    private fun translateReadStat(node: ReadStatementAST) {
+    private fun translateReadStat(state: State, node: ReadStatementAST) {
         TODO()
     }
 
     @TranslatorMethod(NewPairAST::class)
-    private fun translateNewPair(node: NewPairAST) {
+    private fun translateNewPair(state: State, node: NewPairAST) {
         TODO()
     }
 
     @TranslatorMethod(IntLiteralAST::class)
-    private fun translateIntLiteral(node: IntLiteralAST) {
+    private fun translateIntLiteral(state: State, node: IntLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(BoolLiteralAST::class)
-    private fun translateBoolLiteral(node: BoolLiteralAST) {
+    private fun translateBoolLiteral(state: State, node: BoolLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(CharLiteralAST::class)
-    private fun translateCharLiteral(node: CharLiteralAST) {
+    private fun translateCharLiteral(state: State, node: CharLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(StringLiteralAST::class)
-    private fun translateStringLiteral(node: StringLiteralAST) {
+    private fun translateStringLiteral(state: State, node: StringLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(VariableIdentifierAST::class)
-    private fun translateVariableIdentifier(node: VariableIdentifierAST) {
+    private fun translateVariableIdentifier(state: State, node: VariableIdentifierAST) {
         TODO()
     }
 
     @TranslatorMethod(NullPairLiteralAST::class)
     @Suppress("UNUSED_PARAMETER")
-    private fun translateNullPairLiteralAST(node: NullPairLiteralAST) {
+    private fun translateNullPairLiteralAST(state: State, node: NullPairLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(ArrayLiteralAST::class)
-    private fun translateArrayLiteral(node: ArrayLiteralAST) {
+    private fun translateArrayLiteral(state: State, node: ArrayLiteralAST) {
         TODO()
     }
 
     @TranslatorMethod(ArrayElemAST::class)
-    private fun translateArrayElem(node: ArrayElemAST) {
+    private fun translateArrayElem(state: State, node: ArrayElemAST) {
         TODO()
     }
 
     @TranslatorMethod(AssignToArrayElemAST::class)
-    private fun translateAssignToArrayElem(node: AssignToArrayElemAST) {
+    private fun translateAssignToArrayElem(state: State, node: AssignToArrayElemAST) {
         TODO()
     }
 
     @TranslatorMethod(FstPairElemAST::class)
-    private fun translateFstPairElem(node: FstPairElemAST) {
+    private fun translateFstPairElem(state: State, node: FstPairElemAST) {
         TODO()
     }
 
     @TranslatorMethod(SndPairElemAST::class)
-    private fun translateSndPairElem(node: SndPairElemAST) {
+    private fun translateSndPairElem(state: State, node: SndPairElemAST) {
         TODO()
     }
 
     @TranslatorMethod(AssignToPairElemAST::class)
-    private fun translateAssignToPairElem(node: AssignToPairElemAST) {
+    private fun translateAssignToPairElem(state: State, node: AssignToPairElemAST) {
         TODO()
     }
 
     @TranslatorMethod(UnaryOpExprAST::class)
-    private fun translateUnOp(unOpExpr: UnaryOpExprAST) {
+    private fun translateUnOp(state: State, node: UnaryOpExprAST) {
         TODO()
     }
 
     @TranslatorMethod(BinaryOpExprAST::class)
-    private fun translateBinOp(expr: BinaryOpExprAST) {
+    private fun translateBinOp(state: State, node: BinaryOpExprAST) {
         TODO()
+    }
+
+    private fun log(str: String) {
+        if (enableLogging) {
+            println(str)
+        }
     }
 }
