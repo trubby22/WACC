b48afeceaa150717cd88a8d6ca974506112b4ddc
Khemi <kjc20@ic.ac.uk>
2022-03-18 02:04:49 +0000

    [kjc20] [feat] perform liveness analysis: compute live variables between each statement

--- src/main/kotlin/ic/doc/group15/ssa/tac/LiveInterval.kt
+++ src/main/kotlin/ic/doc/group15/ssa/tac/LiveInterval.kt
@@ -1,23 +1,15 @@
 package ic.doc.group15.ssa.tac
 
-import ic.doc.group15.ssa.*
+import ic.doc.group15.ssa.BasicBlock
+import ic.doc.group15.ssa.ExitBasicBlock
+import ic.doc.group15.ssa.IRFunction
 
-data class LivenessResult(val dummy: Int)
+data class LivenessResult(
+  val inOfVar: Map<ThreeAddressCode, Set<Var>>,
+  val outOfVar: Map<ThreeAddressCode, Set<Var>>)
 
 class LivenessAnalysis {
   companion object {
-    // Variable defined by instruction
-    private fun varAssignedTo(instruction: ThreeAddressCode): Var? {
-      return when (instruction) {
-        is Allocate -> instruction.reg
-        is AssignBinOp -> instruction.reg
-        is AssignCall -> instruction.reg
-        is AssignValue -> instruction.reg
-        is Load -> instruction.reg
-        else -> null
-      }
-    }
-
     private fun findPred(function: IRFunction): Map<ThreeAddressCode, Set<ThreeAddressCode>> {
       val pred = mutableMapOf<ThreeAddressCode, Set<ThreeAddressCode>>()
       if (function.basicBlocks.isEmpty()) return pred
@@ -87,6 +79,7 @@ class LivenessAnalysis {
       lastInstToFindSucc: ThreeAddressCode?
     ) {
       val instList = block.getInstructionList()
+
       if (instList.isEmpty()) {
         for (succ in block.getSuccessors()) {
           if (succ is ExitBasicBlock) continue
@@ -117,8 +110,51 @@ class LivenessAnalysis {
       }
     }
 
+    /**
+     * Efficient worklist algorithm as defined in
+     * https://groups.seas.harvard.edu/courses/cs153/2019fa/lectures/Lec20-Dataflow-analysis.pdf
+     */
     fun apply(function: IRFunction): LivenessResult {
-      TODO()
+      val predOf = findPred(function)
+      val succOf = findSucc(function)
+
+      // Confirm the number of instructions is same
+      assert(predOf.size == succOf.size)
+
+      val allInsts = predOf.keys
+
+      // Initialise set of variables live on entry for each instruction
+      val inOf = allInsts.associateBy({ it },
+        { emptySet<Var>() }).toMutableMap()
+      // Initialise set of variables live on exit for each instruction
+      val outOf = HashMap(inOf)
+
+      // Use a FIFO queue of instructions to be updated
+      val worklist = ArrayDeque(allInsts)
+
+      while (worklist.isNotEmpty()) {
+        val inst = worklist.removeFirst()
+
+        // Remember old inOf[inst]
+        val oldIn = inOf[inst]!!
+
+        // out[inst] := for all inst' in succ[inst], the result of union of in[inst']
+        val newOut = succOf[inst]!!.fold(emptySet<Var>()) { acc, elem -> acc union inOf[elem]!! }
+        outOf[inst] = newOut
+
+        // in[inst] := use[inst] union (out[inst] - def[inst])
+        val newIn = inst.usesSet() union (newOut subtract inst.definesSet())
+        inOf[inst] = newIn
+
+        // If worklist has changed, add new work to worklist
+        if (oldIn != inOf[inst]) {
+          for (pred in predOf[inst]!!) {
+            worklist.addLast(pred)
+          }
+        }
+      }
+
+      return LivenessResult(inOf, outOf)
     }
   }
 }
--- src/main/kotlin/ic/doc/group15/ssa/tac/ThreeAddressCode.kt
+++ src/main/kotlin/ic/doc/group15/ssa/tac/ThreeAddressCode.kt
@@ -7,7 +7,17 @@ import ic.doc.group15.ssa.Successor
  * Quadruple form - three address code maintains an unlimited amount of registers,
  * representing either declared variables or temporaries
  */
-sealed interface ThreeAddressCode
+sealed interface ThreeAddressCode {
+    /**
+     * Set of variables used by the current instruction.
+     */
+    fun usesSet(): Set<Var>
+
+    /**
+     * Set of variables defined by the current instruction.
+     */
+    fun definesSet(): Set<Var>
+}
 
 /**
  * Assignments
@@ -17,18 +27,27 @@ data class AssignBinOp(
     val op: BinaryOp,
     val x: Operand,
     val y: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(x, y).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 data class AssignValue(
     val reg: Var,
     val x: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(x).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 class AssignCall(
     val reg: Var,
     val f: Func,
     vararg val args: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = args.filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 /**
  * (Void) function calls
@@ -36,7 +55,10 @@ class AssignCall(
 class Call(
     val f: Func,
     vararg val args: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = args.filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = emptySet()
+}
 
 /**
  * Branch statements
@@ -45,11 +67,17 @@ class Call(
 data class BranchIf(
     val cond: Operand,
     val block: Successor
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(cond).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = emptySet()
+}
 
 data class Branch(
     val block: Successor
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = emptySet()
+    override fun definesSet(): Set<Var> = emptySet()
+}
 
 /**
  * Memory instructions (used for heap allocations/value storing)
@@ -57,23 +85,35 @@ data class Branch(
 data class Allocate(
     val reg: Var,
     val amount: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(amount).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 data class Load(
     val reg: Var,
     val x: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(x).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 data class Store(
     val reg: Var,
     val x: Operand
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = setOf(x).filterIsInstance<Var>().toSet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
 data class Argument(
     val reg: Var
-) : ThreeAddressCode
+) : ThreeAddressCode {
+    override fun usesSet(): Set<Var> = emptySet()
+    override fun definesSet(): Set<Var> = setOf(reg)
+}
 
-/**
- * Pseudo-instruction for SSA form
- */
-data class Phi(val reg: Var, val args: Collection<Operand>) : ThreeAddressCode
+///**
+// * Pseudo-instruction for SSA form
+// */
+//data class Phi(val reg: Var, val args: Collection<Operand>) : ThreeAddressCode
